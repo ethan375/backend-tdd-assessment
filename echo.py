d9730f3e69ea88c7cca94f9893584f652225babc
ethan375 <ethanebel1@gmail.com>
2019-09-26 13:18:13 -0400

    finished

--- echo.py
+++ echo.py
@@ -2,21 +2,40 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility"""
 
-__author__ = "???"
+__author__ = "Ethan375"
 
 
 import sys
+import argparse
+import subprocess
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    
+    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
+    parser.add_argument('text', help="text to be manipulated")
+    parser.add_argument('-u', "--upper", action="store_true", help="convert text to uppercase")
+    parser.add_argument('-l', "--lower", action="store_true", help="convert text to lowercase")
+    parser.add_argument('-t', "--title", action="store_true", help="convert text to titlecase")
+
+    args = parser.parse_args()
+
+    main(args)
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    echo_string = args.text
 
+    if args.lower:
+        echo_string = echo_string.lower()
+    elif args.upper:
+        echo_string = echo_string.upper()
+    elif args.title:
+        echo_string = echo_string.title()
+
+    
+    subprocess.call(['echo', echo_string])
 
 if __name__ == '__main__':
-    pass
+    create_parser()
